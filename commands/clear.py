0dd37bcd115e66ad054906d444368cc4402877d7
sperg <58081117+spergmoment@users.noreply.github.com>
2020-05-01 15:37:51 -0400

    dynamics

--- commands/clear.py
+++ commands/clear.py
@@ -1,16 +1,19 @@
-async def run(args, g, c, m, botperms, userperms):
+async def run(**kwargs):
+    g = kwargs['g']
+    c = kwargs['c']
+    m = kwargs['m']
     if not g.me.guild_permissions.manage_messages:
-        return await c.send(botperms('manage messages'))
+        return await c.send(kwargs['botperms']('manage messages'))
     if not m.guild_permissions.manage_messages:
-        return await c.send(userperms('manage_messages'))
+        return await c.send(kwargs['userperms']('manage_messages'))
     if not g.me.guild_permissions.read_message_history:
-        return await c.send(botperms('read message history'))
+        return await c.send(kwargs['botperms']('read message history'))
     if not m.guild_permissions.read_message_history:
-        return await c.send(userperms('read_message_history'))
-    if len(args) < 1:
+        return await c.send(kwargs['userperms']('read_message_history'))
+    if len(kwargs['args']) < 1:
         amt = 20
     else:
-        amt = int(args[0])
+        amt = int(kwargs['args'][0])
     if amt > 10000 or amt < 2:
         return await c.send("Please use a valid amount between 2 and 10000.")
     try:
