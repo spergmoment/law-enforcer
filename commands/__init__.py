87041870abd86b4a0acf1aa654abe6df2a4cdc53
sperg <58081117+spergmoment@users.noreply.github.com>
2020-05-01 15:44:19 -0400

    wow we werent

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,1 +1,15 @@
-# Another init file for imports.
+# Another init file.
+from . import ban, bash, clear, eval, help, info, kick, mute, ping, restart, unban, unmute
+
+banC = ban.run
+bashC = bash.run
+clearC = clear.run
+evalC = eval.run
+helpC = help.run
+infoC = info.run
+kickC = kick.run
+muteC = mute.run
+pingC = ping.run
+restartC = restart.run
+unbanC = unban.run
+unmuteC = unmute.run
