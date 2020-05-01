f30a7973228ea52534f21ea1135a9ab7c0f0897c
sperg <58081117+spergmoment@users.noreply.github.com>
2020-05-01 15:40:13 -0400

    quite

--- commands/ping.py
+++ commands/ping.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-async def run(client, c):
+async def run(**kwargs):
     # get the current time
     start = datetime.now()
     # send the client ping
-    message = await c.send(f"Client Ping: {round(client.latency*1000)}")
+    message = await kwargs['c'].send(f"Client Ping: {round(kwargs['client'].latency*1000)}")
     # then add the current time - the start time
-    await message.edit(content=f"Client Ping: {round(client.latency*1000)}\nAPI Ping: {round((datetime.now().microsecond-start.microsecond)/1000)}")
+    await message.edit(content=f"{message.content}\nAPI Ping: {round((datetime.now().microsecond-start.microsecond)/1000)}")
