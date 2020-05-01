e34a1c2960a60353b0eb6924dec6bd58c0fc0a76
sperg <58081117+spergmoment@users.noreply.github.com>
2020-05-01 13:31:11 -0400

    restart cmd change

--- constants/info.py
+++ constants/info.py
@@ -1,7 +1,7 @@
 server = "https://discord.gg/PVTBgK6"
 srccode = "https://github.com/spergmoment/law-enforcer"
 oauth = "https://discordapp.com/api/oauth2/authorize?client_id=696124534679535728&permissions=268561591&scope=bot"
-restart = "pm2 restart 3"
+restart = "pm2 restart 7"
 
 staticinfo = f"```Owner: literal monkey#9193\nOwner ID: 694643777146454096\n"
 f"Running on: 5.6.3-arch1-1 x86_64 64 bit\nProcessors: 4 × Intel Core i5-3210M CPU @ 2.50GHz\nMemory: 7.7 GB"
