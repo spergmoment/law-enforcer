8a9053c4d56fa91c12c9f41d6eb22f9344304b27
sperg <58081117+spergmoment@users.noreply.github.com>
2020-05-01 15:36:02 -0400

    dynamic stuff

--- commands/ban.py
+++ commands/ban.py
@@ -1,29 +1,28 @@
-
-
-async def run(args, msg, g, c, m, botlower, userlower, botperms, userperms):
-    # checks bot perms, see constants
-    if not g.me.guild_permissions.ban_members:
-        return await c.send(botperms('ban members'))
+async def run(**kwargs):
+    c = kwargs['c']
+    m = kwargs['m']
+    if not kwargs['g'].me.guild_permissions.ban_members:
+        return await c.send(kwargs['botperms']('ban members'))
     # checks user perms, see constants
     if not m.guild_permissions.ban_members:
-        return await c.send(userperms('ban_members'))
+        return await c.send(kwargs['userperms']('ban_members'))
     # checks for mentions
-    if not msg.mentions:
+    if not kwargs['msg'].mentions:
         return await c.send("Please provide a member to ban.")
     # uses the first mention
-    member = msg.mentions[0]
+    member = kwargs['msg'].mentions[0]
     # the reason, or "None"
-    reason = " ".join(args[1:len(args)]) or "None"
+    reason = " ".join(kwargs['args'][1:len(args)]) or "None"
     # checks role hierarchy, see constants
-    if g.me.top_role < member.top_role:
-        return await c.send(botlower)
+    if kwargs['g'].me.top_role < member.top_role:
+        return await c.send(kwargs['botlower'])
     if m.top_role < member.top_role:
-        return await c.send(userlower)
+        return await c.send(kwargs['userlower'])
     # try block
     try:
         # try to tell the member they've been banned
         try:
-            await member.send(f"{member}, you have been **banned** from {g} by {m}.\nReason: {reason}")
+            await member.send(f"{member}, you have been **banned** from {kwargs['g']} by {m}.\nReason: {reason}")
         # if it doesn't work, ignore it and move on
         except:
             pass
