2ee769a6e6759d6b61dedcb0b81db8478538f2b6
sperg <58081117+spergmoment@users.noreply.github.com>
2020-05-01 15:39:06 -0400

    very fun

--- commands/info.py
+++ commands/info.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-async def run(client, c, startTime, staticinfo, endinfo):
+async def run(**kwargs):
     # staticinfo and endinfo are used to shorten this a bit, see constants
     # the uptime is just the current total of seconds it's been up
-    await c.send(f"{staticinfo}\nCurrent uptime: "
-    f"{round((datetime.now()-startTime).total_seconds())} seconds\nCurrent latency: {round(client.latency*1000)}```\n{endinfo}")
+    await kwargs['c'].send(f"{kwargs['staticinfo']}\nCurrent uptime: "
+    f"{round((datetime.now()-kwargs['startTime']).total_seconds())} "
+    f"seconds\nCurrent latency: {round(kwargs['client'].latency*1000)}```\n{kwargs['endinfo']}")
